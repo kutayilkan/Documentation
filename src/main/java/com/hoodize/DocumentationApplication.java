7b625102bebf2fa9a026dabcf90f933f15b025c0
Melik Kutay İlkan <melikkutayilkan@Melik-MacBook-Pro.local>
2024-09-28 19:12:50 +0300

    test commit

--- src/main/java/com/hoodize/DocumentationApplication.java
+++ src/main/java/com/hoodize/DocumentationApplication.java
@@ -8,6 +8,7 @@ public class DocumentationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DocumentationApplication.class, args);
+		// System.out.println("Hello, World!");
 	}
 
 }
